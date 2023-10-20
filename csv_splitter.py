43ebf1c30847afb8afc43855d8f1a99ee1a36c7f
Dan Alvarez <82dannyalvarez@gmail.com>
2023-10-20 16:58:06 -0400

    Update csv_splitter.py

--- csv_splitter.py
+++ csv_splitter.py
@@ -1,23 +1,37 @@
-# USING AMAZON CODEWHISPERER: https://www.codewhisperer.com/
-# I want to write a Lambda function for splitting a CSV file into chunks, which should be stored in other CSV files. Each file should be uploaded to an S3 bucket. Please use the Pandas library for the chunking.
-
 import pandas as pd
 import os
 import boto3
+import json
 
-def split_csv_async(file_name, chunk_size, bucket_name, s3_client):
-    df = pd.read_csv(file_name)
-    for i in range(0, len(df), chunk_size):
-        chunk = df.iloc[i:i+chunk_size]
-        chunk.to_csv(f"{file_name[:-4]}_chunk{i//chunk_size}.csv", index=False)
-        s3_client.upload_file(f"{file_name[:-4]}_chunk{i//chunk_size}.csv", bucket_name, f"{file_name[:-4]}_chunk{i//chunk_size}.csv")
-        os.remove(f"{file_name[:-4]}_chunk{i//chunk_size}.csv")
-        print(f"Chunk {i//chunk_size} uploaded to S3 bucket {bucket_name}")
-
-# write the code to create an S3 client
 s3_client = boto3.client('s3')
 
-# write the code to call the above function
-split_csv_async("sample_data/annual-enterprise-survey-2021-financial-year-provisional-csv.csv", 10000, "working-directory", s3_client)
+def lambda_handler(event, context):
+    for record in event['Records']:
+        #pull the body out & json load it
+        json_record = (record["body"])
+        json_record = json.loads(json_record)
+        object_key = json_record['Records'][0]['s3']['object']['key']
+        upload_bucket = json_record['Records'][0]['s3']['bucket']['name']
+        chunk_size = 10000
+        
+        # code to load the working_bucket environment varible
+        working_bucket = os.environ['working_bucket']
+        
+        # given the bucket name and object name, get the object from s3
+        file_name = f"/tmp/{object_key}"
+        s3_client.download_file(upload_bucket, object_key, file_name)
+    
+        df = pd.read_csv(f"{file_name}")
+        for i in range(0, len(df), chunk_size):
+            chunk = df.iloc[i:i+chunk_size]
+            chunk.to_csv(f"{file_name[:-4]}_chunk{i//chunk_size}.csv", index=False)
+            s3_client.upload_file(f"{file_name[:-4]}_chunk{i//chunk_size}.csv", working_bucket, f"{object_key[:-4]}/{file_name[5:-4]}_chunk_{i//chunk_size}.csv")
+            os.remove(f"{file_name[:-4]}_chunk{i//chunk_size}.csv")
+            print(f"Chunk {i//chunk_size} uploaded to S3 bucket {working_bucket}")
+            
+    return {
+        'statusCode': 200,
+        'body': json.dumps(f"File(s) have been split and uploaded to {working_bucket}.")
+    }
 
 
